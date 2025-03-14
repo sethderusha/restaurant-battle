c9e4ef637cff6dca1dd817f0ea024d426cc3fb35
Seth DeRusha <srd6665@nyu.edu>
2025-03-14 17:51:00 -0400

    warnings removed

--- components/LoadingOverlay.tsx
+++ components/LoadingOverlay.tsx
@@ -33,13 +33,7 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    boxShadow: '0px 2px 3.84px rgba(0, 0, 0, 0.25)',
     elevation: 5,
   },
   message: {
