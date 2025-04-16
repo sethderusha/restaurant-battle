9bedadb8ae672141cef2feb8d12684355f352ddd
Milena Synek <msynek@pacbell.net>
2025-04-16 12:38:21 -0400

    oopsies

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 // API configuration
-export const API_URL = 'http://localhost:5001/api'; 
-// export const API_URL = 'https://backend-lg9u.onrender.com/api';
+// export const API_URL = 'http://localhost:5001/api'; 
+export const API_URL = 'https://backend-lg9u.onrender.com/api';
